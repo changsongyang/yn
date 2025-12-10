5f7d6c1636bf5a351613fd70a7faf3d30128369e
purocean <purocean@gmail.com>
2025-12-11 00:04:38 +0800

    feat: replace Xterm component with Terminal and refactor related code

--- src/renderer/components/Xterm.vue
+++ src/renderer/components/Xterm.vue
@@ -1,30 +1,20 @@
 <template>
-  <div class="xterm">
-    <div class="hide" @click="$emit('hide')">
-      <SvgIcon style="width: 20px; height: 20px" name="chevron-down" />
-    </div>
-    <div ref="refXterm" style="height: 100%"></div>
-  </div>
+  <div class="xterm" ref="domRef" style="height: 100%; width: 100%;" />
 </template>
 
 <script lang="ts">
 import io, { Socket } from 'socket.io-client'
-import { defineComponent, nextTick, onBeforeMount, onBeforeUnmount, ref } from 'vue'
+import { defineComponent, onBeforeUnmount, ref } from 'vue'
 import { Terminal } from 'xterm'
 import { FitAddon } from 'xterm-addon-fit'
 import { WebLinksAddon } from 'xterm-addon-web-links'
 import { OneHalfLight, OneHalfDark } from 'xterm-theme'
-import { getLogger, sleep } from '@fe/utils'
+import { getLogger } from '@fe/utils'
 import { registerHook, removeHook } from '@fe/core/hook'
-import { registerAction, removeAction } from '@fe/core/action'
 import { $args, FLAG_DEMO, FLAG_DISABLE_XTERM } from '@fe/support/args'
-import { toggleXterm } from '@fe/services/layout'
 import { getColorScheme } from '@fe/services/theme'
 import { t } from '@fe/services/i18n'
-import { isWindows } from '@fe/support/env'
-import type { BuildInActions } from '@fe/types'
-import store from '@fe/support/store'
-import SvgIcon from './SvgIcon.vue'
+import type { Components } from '@fe/types'
 
 import 'xterm/css/xterm.css'
 
@@ -32,21 +22,19 @@ const logger = getLogger('component-x-term')
 
 export default defineComponent({
   name: 'xterm',
-  components: { SvgIcon },
   setup () {
-    const refXterm = ref<HTMLElement | null>(null)
+    const domRef = ref<HTMLElement | null>(null)
 
     let xterm: Terminal | null = null
     // eslint-disable-next-line no-undef
     let socket: Socket | null = null
+    let resizeObserver: ResizeObserver | null = null
 
-    const fitAddon = new FitAddon()
-    const webLinksAddon = new WebLinksAddon()
+    let fitAddon: FitAddon | null = null
+    let webLinksAddon: WebLinksAddon | null = null
 
     function fitXterm () {
-      if (store.state.showXterm) {
-        fitAddon.fit()
-      }
+      fitAddon?.fit()
     }
 
     function changeTheme () {
@@ -57,14 +45,14 @@ export default defineComponent({
     }
 
     function input (data: string) {
-      socket!.emit('input', data)
+      socket?.emit('input', data)
     }
 
     function focus () {
-      xterm!.focus()
+      xterm?.focus()
     }
 
-    function init (opts?: Parameters<BuildInActions['xterm.init']>['0']) {
+    function init (opts?: Components.XTerm.InitOpts) {
       if (FLAG_DISABLE_XTERM) {
         logger.warn('xterm disabled')
         return
@@ -74,21 +62,35 @@ export default defineComponent({
         xterm = new Terminal({
           cols: 80,
           rows: 24,
-          fontSize: 18,
+          fontSize: 16,
           cursorStyle: 'underline',
           // fontFamily: 'Consolas',
           fontWeightBold: '500',
+          ...opts
         })
 
         changeTheme()
 
+        fitAddon = new FitAddon()
+        webLinksAddon = new WebLinksAddon()
+
         xterm.loadAddon(fitAddon)
         xterm.loadAddon(webLinksAddon)
 
-        xterm.open(refXterm.value!)
+        xterm.open(domRef.value!)
         fitAddon.fit()
         registerHook('THEME_CHANGE', changeTheme)
-        registerHook('GLOBAL_RESIZE', fitXterm)
+
+        resizeObserver = new ResizeObserver((entires) => {
+          const entry = entires[0]
+          // do not fit when size too small
+          if (entry.contentRect.width < 5 || entry.contentRect.height < 5) {
+            return
+          }
+
+          fitXterm()
+        })
+        resizeObserver.observe(domRef.value!)
 
         if (FLAG_DEMO) {
           const message = t('demo-tips')
@@ -102,7 +104,7 @@ export default defineComponent({
       }
 
       const query = {
-        cwd: opts?.cwd || store.state.currentRepo?.path || ''
+        cwd: opts?.cwd || ''
       }
 
       if (!socket) {
@@ -116,8 +118,8 @@ export default defineComponent({
         xterm.onData(input)
         socket.on('output', (arrayBuffer: any) => xterm!.write(arrayBuffer))
         socket.on('disconnect', () => {
-          xterm!.clear()
-          toggleXterm(false)
+          dispose()
+          opts?.onDisconnect?.()
         })
       }
 
@@ -132,106 +134,47 @@ export default defineComponent({
       focus()
     }
 
-    async function runInXterm (cmd: { code: string, start?: string, exit?: string } | string) {
-      if (FLAG_DISABLE_XTERM) {
-        logger.warn('xterm disabled')
-        return
+    function dispose () {
+      if (resizeObserver) {
+        resizeObserver.disconnect()
+        resizeObserver = null
       }
 
-      toggleXterm(true)
-
-      await nextTick()
-
-      init()
-
-      const eol = isWindows ? '\r\n' : '\n'
-
-      if (typeof cmd === 'string') {
-        cmd = { code: cmd }
+      if (socket) {
+        socket.disconnect()
+        socket = null
       }
 
-      if (cmd.start) {
-        input(cmd.start)
-        input(eol)
-        // wait for child process ready.
-        await sleep(400)
+      if (xterm) {
+        xterm.dispose()
+        xterm = null
       }
 
-      cmd.code.split('\n').forEach(x => {
-        input(x)
-        input(eol)
-      })
+      fitAddon = null
+      webLinksAddon = null
 
-      if (cmd.exit) {
-        input(cmd.exit)
-        input(eol)
-      }
+      removeHook('THEME_CHANGE', changeTheme)
     }
 
-    onBeforeMount(() => {
-      registerAction({ name: 'xterm.run', handler: runInXterm })
-      registerAction({ name: 'xterm.init', handler: init })
-    })
-
     onBeforeUnmount(() => {
-      removeAction('xterm.run')
-      removeAction('xterm.init')
-      removeHook('THEME_CHANGE', changeTheme)
-      removeHook('GLOBAL_RESIZE', fitXterm)
+      dispose()
     })
 
     return {
-      refXterm,
-    }
+      domRef,
+      init,
+      input,
+      fit: fitXterm,
+      dispose,
+      getXterm: () => xterm,
+      getSocket: () => socket,
+    } satisfies Components.XTerm.Ref
   },
 })
 </script>
 
 <style scoped>
-.xterm {
-  box-sizing: border-box;
-  padding: 5px;
-  background: var(--g-color-98);
-  border: 1px solid var(--g-color-88);
-  flex: 0 0 auto;
-  width: 100%;
-  height: 100%;
-  padding-right: 0;
-  padding-top: 0;
-  border-right: 0;
-}
-
 .xterm ::v-deep(textarea) {
   transition: none;
-}
-
-.hide {
-  position: absolute;
-  top: -6px;
-  right: 0;
-  z-index: 10;
-  background: var(--g-color-75);
-  border-bottom-left-radius: var(--g-border-radius);
-  border-bottom-right-radius: var(--g-border-radius);
-  width: 30px;
-  height: 30px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  transform: scaleY(0.6);
-  border: 8px solid transparent;
-  box-sizing: border-box;
-  color: var(--g-color-20);
-  opacity: 0;
-  transition: opacity .2s;
-}
-
-.xterm:hover .hide {
-  opacity: 1;
-}
-
-.hide:hover {
-  background: var(--g-color-65);
 }
 </style>
